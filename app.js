682ea65c19075117f5713f530044fbfa7d5ba04e
audibleblink <virtualsnyper@gmail.com>
2015-07-07 14:48:30 -0400

    add livestream api wrapper
    - add 'requests' dependancy
    - code styling edits

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express')
-var app = express()
+var express      = require('express')
 var cookieParser = require('cookie-parser')
 var bodyParser   = require('body-parser')
-var routes = require('./routes/directors.js')
+var routes       = require('./routes/directors.js')
+var app          = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
---
+++ ls-api.js
@@ -0,0 +1,14 @@
+var request = require('request')
+var apiUrl = 'https://api.new.livestream.com/accounts/'
+
+module.exports = {
+  get: function(accountId, cb){
+    request(apiUrl + accountId, function(err, res, body){
+      if (!err && res.statusCode == 200) {
+        cb(body)
+      } else {
+        console.log(res, err)
+      }
+    })
+  }
+}
