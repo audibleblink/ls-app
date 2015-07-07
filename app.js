4c31c034e7065989d995de4071c201bbd8188c4d
audibleblink <virtualsnyper@gmail.com>
2015-07-07 17:34:48 -0400

    add redis connection to app

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var bodyParser   = require('body-parser')
 var routes       = require('./routes/directors.js')
 var app          = express()
 
+var Director = require('./models/director')
+var nohm  = require('nohm').Nohm;
+var redis = require('redis').createClient();
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+nohm.setClient(redis)
+app.use(nohm.connect([{
+  model: Director
+}]))
 
 app.use('/', routes)
 
