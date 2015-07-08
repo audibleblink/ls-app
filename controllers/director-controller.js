6bbd120583b40a57cb96be0c97de644a1e2aac7d
audibleblink <virtualsnyper@gmail.com>
2015-07-07 23:31:16 -0400

    break out code into contollers

---
+++ controllers/director-controller.js
@@ -0,0 +1,98 @@
+var lsApi    = require('../ls-api')
+var Director = require('../models/director')
+var nohm     = require('nohm').Nohm;
+
+module.exports = {
+  index: index,
+  show: show,
+  create: create,
+  update: update
+}
+
+function index(req, res){
+  Director.findAndLoad({}, function(err, directors){
+    if (err) {
+      sendError.apply(this, [500, err])
+    } else {
+      var response = directors.map(function(dir){
+        return dir.allProperties()
+      })
+      res.json(response)
+    }
+  })
+}
+
+function show(req, res){
+  Director.findAndLoad({livestream_id: req.params.id}, function(err, items){
+    if (err === 'not found') {
+      sendError.apply(res, [400, err])
+    } else if (err) {
+      sendError.apply(res, [500, err])
+    } else {
+      res.json(items[0].allProperties())
+    }
+  })
+}
+
+function create(req, res){
+  var lsId = req.body.livestream_id
+  lsApi.get(lsId, function(err, body){
+    if (err) {
+      sendError.apply(res, [500, err])
+    } else {
+      var data = {
+        full_name: body.full_name,
+        dob: body.dob,
+        livestream_id: lsId
+      }
+
+      director = nohm.factory("Director")
+      director.p(data)
+
+      director.save(function(err){
+        if (err === "invalid") {
+          sendError.apply(res, [400, director.errors])
+        } else if (err) {
+          sendError.apply(res, [500, err])
+        } else {
+          res.json(director.allProperties())
+        }
+      })
+    }
+  })
+}
+
+function update (req, res){
+  Director.findAndLoad({livestream_id: req.params.id}, function(err, items){
+    if (err) {
+      sendError.apply(res, [500, err])
+    } else {
+      var director = items[0]
+      var attributes = allowedParams(req.body)
+      director.p(attributes)
+
+      director.save(function(err){
+        if (err === "invalid") {
+          sendError.apply(res, [400, director.errors])
+        } else if (err) {
+          sendError.apply(res, [500, err])
+        } else {
+          res.json(director.allProperties())
+        }
+      })
+    }
+  })
+}
+
+
+function sendError(status, msg) {
+  this.status(status)
+  this.json({error: msg})
+}
+
+function allowedParams(attributes) {
+  return {
+    favorite_camera: attributes.favorite_camera,
+    favorite_movies: attributes.favorite_movies
+  }
+}
--- routes/directors.js
+++ routes/directors.js
@@ -1,101 +1,10 @@
 var router   = require('express').Router()
-var lsApi    = require('../ls-api')
-var Director = require('../models/director')
-var nohm     = require('nohm').Nohm;
+var directorCtrl = require('../controllers/director-controller')
 
 router
-  .get("/directors", index)
-  .get("/directors/:id", show)
-  .post("/directors", create)
-  .put("/directors/:id", update)
-
-
-function index(req, res){
-  Director.findAndLoad({}, function(err, directors){
-    if (err) {
-      sendError.apply(this, [500, err])
-    } else {
-      var response = directors.map(function(dir){
-        return dir.allProperties()
-      })
-      res.json(response)
-    }
-  })
-}
-
-function show(req, res){
-  Director.findAndLoad({livestream_id: req.params.id}, function(err, items){
-    if (err) {
-      sendError.apply(res, [500, err])
-    } else {
-      res.json(items[0].allProperties())
-    }
-  })
-}
-
-function create(req, res){
-  var lsId = req.body.livestream_id
-  lsApi.get(lsId, function(err, body){
-    if (err) {
-      sendError.apply(res, [500, err])
-    } else {
-      var data = {
-        full_name: body.full_name,
-        dob: body.dob,
-        livestream_id: lsId
-      }
-
-      director = nohm.factory("Director")
-      director.p(data)
-
-      director.save(function(err){
-        if (err === "invalid") {
-          sendError.apply(res, [400, director.errors])
-        } else if (err) {
-          sendError.apply(res, [500, err])
-        } else {
-          res.json(director.allProperties())
-        }
-      })
-    }
-  })
-}
-
-function update (req, res){
-  Director.findAndLoad({livestream_id: req.params.id}, function(err, items){
-    if (err) {
-      sendError.apply(res, [500, err])
-    } else {
-      var director = items[0]
-      var attributes = allowedParams(req.body)
-      director.p(attributes)
-
-      director.save(function(err){
-        if (err === "invalid") {
-          sendError.apply(res, [400, director.errors])
-        } else if (err) {
-          sendError.apply(res, [500, err])
-        } else {
-          res.json(director.allProperties())
-        }
-      })
-    }
-  })
-}
-
-
-function sendError(status, msg) {
-  this.status(status)
-  this.json({error: msg})
-}
-
-function allowedParams(attributes) {
-  return {
-    favorite_camera: attributes.favorite_camera,
-    favorite_movies: attributes.favorite_movies
-  }
-}
-
-
+  .get("/directors", directorCtrl.index)
+  .get("/directors/:id", directorCtrl.show)
+  .post("/directors", directorCtrl.create)
+  .put("/directors/:id", directorCtrl.update)
 
 module.exports = router
