da65bb997db319f6d91510cfd6e1490886b60a84
audibleblink <virtualsnyper@gmail.com>
2015-07-07 23:38:07 -0400

    cleanup unneeded variable assignment

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var express      = require('express')
+var app      = require('express')()
 var cookieParser = require('cookie-parser')
 var bodyParser   = require('body-parser')
 var routes       = require('./routes/directors.js')
-var app          = express()
 
 var Director = require('./models/director')
 var nohm  = require('nohm').Nohm;
